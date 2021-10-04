1c4623692b10fad1517f257925aae674537977ea
iarmankhan <work.armankhan@gmail.com>
2021-10-04 09:59:31 +0530

    v2

--- App.tsx
+++ App.tsx
@@ -1,24 +1,56 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {Dimensions, SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 const {width} = Dimensions.get('window')
 
 export default function App() {
+    const data = Array(20).fill("Hello")
     return (
         <SafeAreaView style={styles.container}>
-
-            <View style={styles.scrollView}>
-                {/*<View style={styles.container}>*/}
+            <ScrollView>
                 {
-                    Array(20).fill("Hello").map((txt, idx) => (
-                        <View style={{paddingHorizontal: 20}} key={idx}>
-                            <Text>{txt}</Text>
-                        </View>
-                    ))
+                    data.map((txt, idx) => {
+                        return (
+                            <View
+                                key={idx}
+                                style={{
+                                    width: 120,
+                                    height: 40,
+                                    backgroundColor: idx % 2 === 0 ?  'blue' : 'yellow',
+                                    transform: [{rotate: '90deg'}, {translateX: 25}, {translateY: 40}],
+                                    alignItems: "center",
+                                    justifyContent:'center',
+                                    marginBottom: 80
+                                }}
+                            >
+                                <Text style={{color: idx % 2 === 0 ? 'white' : 'black'}}>{idx % 2 === 0 ? "Hello There" : "Hello"}</Text>
+                            </View>
+                        )
+                    })
                 }
-                {/*</View>*/}
-            </View>
+
+            </ScrollView>
+            {/*<FlatList*/}
+            {/*    style={{*/}
+
+            {/*    }}*/}
+            {/*    data={data}*/}
+            {/*    keyExtractor={(item, index) => `${item}${index}`}*/}
+            {/*    renderItem={({item, index}) => {*/}
+            {/*        return (*/}
+            {/*            <View style={{*/}
+            {/*                // position: 'relative',*/}
+            {/*                // transform: [{rotate: '90deg'}],*/}
+            {/*                width: 100,*/}
+            {/*                height: 60,*/}
+            {/*                backgroundColor:  index % 2 === 0 ? 'red' : 'yellow'*/}
+            {/*            }}>*/}
+            {/*                <Text>{item}</Text>*/}
+            {/*            </View>*/}
+            {/*        )*/}
+            {/*    }}*/}
+            {/*/>*/}
         </SafeAreaView>
     );
 }
